e3d84df14e7004b256dce938e5247a23822efb5b
surF <surf@libremente.eu>
2016-08-02 00:16:14 +0200

    First push

---
+++ scavenger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from Tkinter import *
+import tkMessageBox
+
+def GetValue():
+    enter = ent.get()
+    if enter == 'Yes':
+        button['bg'] = 'yelloww'
+        ent.delete(0, 'end')
+        tkMessageBox.showinfo("Greaat Student!", "The world is better now!")
+    else:
+        ent.delete(0, 'end')
+        tkMessageBox.showinfo("Mmm", "I thInk I don't like you!")
+
+root = Tk()
+
+lab = Label(root, text = 'Is this workshop interesting? Write Yes or No')
+ent = Entry(root, bg = 'white')
+button = Button(root, text = 'Enter Reply', command = GetValue)
+
+ent.focus()
+
+lab.pack(anchor = W)
+ent.pack(anchor = W)
+button.pack(ancho = E)
+
+root.mainloop()
